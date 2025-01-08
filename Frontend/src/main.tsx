a629cd039e0efedf3456c4c161e5297dc09d98f5
psychlone77 <nithikasdias@gmail.com>
2025-01-08 16:57:43 +0530

    Refactor routing by extracting routes into a separate AppRouter component and removing unused App component and CSS

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,19 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import LoginPage from './pages/LoginPage.tsx'
-import PrivateRoute from './pages/PrivateRoute.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
-import FishFarmPage from './pages/FishFarmPage.tsx'
-import Layout from './layout.tsx'
 import { CssBaseline } from '@mui/material'
-import WorkerPage from './pages/WorkerPage.tsx'
-import NotFoundPage from './pages/NotFoundPage.tsx'
-import LogoutPage from './pages/LogoutPage.tsx'
+import AppRouter from './router.tsx'
 
 const theme = createTheme({
   palette: {
@@ -39,23 +31,7 @@ createRoot(document.getElementById('root')!).render(
       <ReactQueryDevtools initialIsOpen={false} />
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {/* TODO: Extract the router to a different component */}
-        <BrowserRouter>
-          <Routes>
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/logout' element={<LogoutPage />} />
-            <Route path='/' element={<PrivateRoute element={<Layout />} />}>
-              <Route index element={<App />} />
-                <Route
-                path='fish-farms/:fishFarmId'
-                element={<FishFarmPage />}
-                />
-                <Route path='fish-farms/:fishFarmId/workers/:workerId' element={<WorkerPage />} />
-              {/* TODO: Add sign up page */}
-            </Route>
-            <Route path='*' element={<NotFoundPage />} />
-          </Routes>
-        </BrowserRouter>
+        <AppRouter/>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
---
+++ Frontend/src/router.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import LoginPage from './pages/LoginPage'
+import LogoutPage from './pages/LogoutPage'
+import PrivateRoute from './pages/PrivateRoute'
+import Layout from './layout'
+import HomePage from './pages/HomePage'
+import FishFarmPage from './pages/FishFarmPage'
+import WorkerPage from './pages/WorkerPage'
+import NotFoundPage from './pages/NotFoundPage'
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/logout' element={<LogoutPage />} />
+        <Route path='/' element={<PrivateRoute element={<Layout />} />}>
+          <Route index element={<HomePage />} />
+          <Route path='fish-farms/:fishFarmId' element={<FishFarmPage />} />
+          <Route path='fish-farms/:fishFarmId/workers/:workerId' element={<WorkerPage />} />
+        </Route>
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
