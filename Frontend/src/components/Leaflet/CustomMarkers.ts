fe3380d57b26f14043fe58e66f805e85966dff0e
Nithika Dias <nithikasdias@gmail.com>
2025-02-04 19:18:40 +0530

    Import url from top, support fix build bug

--- Frontend/src/components/Leaflet/CustomMarkers.ts
+++ Frontend/src/components/Leaflet/CustomMarkers.ts
@@ -1,17 +1,19 @@
 import L from 'leaflet';
+import fishfarm from '/fish-farm.webp';
+import boat from '/boat.png';
 
 export const markerIcon = new L.Icon({
-    iconUrl: '/fish-farm.webp',
-    iconSize:    [41, 41],
-    iconAnchor:  [19, 41],
+    iconUrl: fishfarm,
+    iconSize: [41, 41],
+    iconAnchor: [19, 41],
     popupAnchor: [1, -34],
     tooltipAnchor: [16, -28],
 });
 
 export const boatMarkerIcon = new L.Icon({
-    iconUrl: '/boat.png',
-    iconSize:    [41, 41],
-    iconAnchor:  [18, 41],
+    iconUrl: boat,
+    iconSize: [41, 41],
+    iconAnchor: [18, 41],
     popupAnchor: [0, -34],
     tooltipAnchor: [19, -28],
 });
